daff967827f44853ae1ee3088e4bfa9fe5ab5e7c
victord <victordaniel.123rt@gmail.com>
2026-07-28 12:31:52 -0600

    Validation

--- src/main/java/com/vdgarcia/repairShop_service/model/dto/ClienteDTO.java
+++ src/main/java/com/vdgarcia/repairShop_service/model/dto/ClienteDTO.java
@@ -1,5 +1,9 @@
 package com.vdgarcia.repairShop_service.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.List;
@@ -12,9 +16,17 @@ import java.util.List;
 public class ClienteDTO {
 
     private Long id;
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
     private String nombre;
+    @NotBlank(message = "El apellido es obligatorio")
+    @Size(min = 2, max = 50, message = "El apellido debe tener entre 2 y 50 caracteres")
     private String apellido;
+    @NotBlank(message = "El teléfono es obligatorio")
+    @Pattern(regexp = "^\\+?[0-9]{7,15}$", message = "El teléfono debe contener entre 7 y 15 dígitos y opcionalmente el prefijo '+'")
     private String telefono;
+    @NotBlank(message = "El correo es obligatorio")
+    @Email(message = "El formato del correo electrónico no es válido")
     private String correo;
     private List<VehiculoDTO> vehiculos;
 }
