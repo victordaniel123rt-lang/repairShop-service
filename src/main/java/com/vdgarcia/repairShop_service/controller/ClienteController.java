8404f86176e4120ad290318dbf354d72598d022e
victord <victordaniel.123rt@gmail.com>
2026-07-28 12:47:40 -0600

    Validation

--- src/main/java/com/vdgarcia/repairShop_service/controller/ClienteController.java
+++ src/main/java/com/vdgarcia/repairShop_service/controller/ClienteController.java
@@ -2,6 +2,10 @@ package com.vdgarcia.repairShop_service.controller;
 
 import com.vdgarcia.repairShop_service.model.dto.ClienteDTO;
 import com.vdgarcia.repairShop_service.service.intef.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,30 +15,56 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/cliente")
 @RequiredArgsConstructor
+@Tag(name = "Clientes", description = "Controlador para gestion y creación de nuevos clientes")
 public class ClienteController {
     private final ClienteService service;
 
     @GetMapping
+    @Operation(summary = "Listar todos los clientes", description = "Muestra todos los clientes que existen hasta el momento de la consulta")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista desplegada con exito"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos o inconsistentes")
+    })
     public ResponseEntity<List<ClienteDTO>> obtenerTodos(){
         return ResponseEntity.ok(service.obtenerTodos());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Encuentra a un cliente en especifico", description = "Muestra a un elemento en particular con identificador único")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Elemento encontrado y mostrado con exito"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos o inconsistentes")
+    })
     public ResponseEntity<ClienteDTO> obtenerPorId(@PathVariable Long id){
         return ResponseEntity.ok(service.obtenerPorId(id));
     }
 
     @PostMapping
+    @Operation(summary = "Crea un nuevo cliente", description = "Hace el registro de un nuevo cliente. No se debe enviar el Id en el cuerpo del JSON")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cliente creado con éxito"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos o inconsistentes")
+    })
     public ResponseEntity<ClienteDTO> crear(@RequestBody ClienteDTO dto){
         return ResponseEntity.ok(service.crear(dto));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualiza un cliente", description = "Hace la actualización correspondiente de un cliente. Es necesario incluir el id en el cliente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Actualización exitosa"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos o inconsistentes")
+    })
     public ResponseEntity<ClienteDTO> actualizar(@PathVariable Long id, @RequestBody ClienteDTO dto){
         return ResponseEntity.ok(service.actualizar(id,dto));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Elimina un cliente", description = "Elimina un cliente creado con anterioridad. Es necesario el ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200/2004", description = "Elemento eliminado con exito"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos o inconsistentes")
+    })
     public ResponseEntity<ClienteDTO> eliminar (@PathVariable Long id){
         return ResponseEntity.ok(service.eliminar(id));
     }
--- src/main/java/com/vdgarcia/repairShop_service/model/dto/ClienteDTO.java
+++ src/main/java/com/vdgarcia/repairShop_service/model/dto/ClienteDTO.java
@@ -1,5 +1,6 @@
 package com.vdgarcia.repairShop_service.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -13,20 +14,25 @@ import java.util.List;
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Builder
+@Schema(description = "Modelo que representa la información de un cliente")
 public class ClienteDTO {
-
+    @Schema(description = "ID único del cliente (autogenerado)", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
     @NotBlank(message = "El nombre es obligatorio")
     @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
+    @Schema(description = "Nombre con el que se identifica el cliente", example = "Víctor", requiredMode = Schema.RequiredMode.REQUIRED)
     private String nombre;
     @NotBlank(message = "El apellido es obligatorio")
     @Size(min = 2, max = 50, message = "El apellido debe tener entre 2 y 50 caracteres")
+    @Schema(description = "Apellido con el que se identifica el cliente", example = "García", requiredMode = Schema.RequiredMode.REQUIRED)
     private String apellido;
     @NotBlank(message = "El teléfono es obligatorio")
     @Pattern(regexp = "^\\+?[0-9]{7,15}$", message = "El teléfono debe contener entre 7 y 15 dígitos y opcionalmente el prefijo '+'")
+    @Schema(description = "Numero telefonico del cliente", example = "555655145")
     private String telefono;
     @NotBlank(message = "El correo es obligatorio")
     @Email(message = "El formato del correo electrónico no es válido")
+    @Schema(description = "email personal del cliente", example = "vgarcia@example.com")
     private String correo;
     private List<VehiculoDTO> vehiculos;
 }
